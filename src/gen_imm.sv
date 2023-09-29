289fb25be95e5781afdbb245e28c2d6af3d191b5
Tom Haber <tom.haber@imec.be>
2023-09-29 15:40:33 +0200

    add imm for lui/auipc

--- src/gen_imm.sv
+++ src/gen_imm.sv
@@ -15,6 +15,7 @@ always @(*) begin
         rvcpu::jalr_offset: imm = {{(Width - 12){op[31]}}, op[31:20]};
         rvcpu::br_offset: imm = {{(Width - 13){op[31]}}, op[31], op[7], op[30:25], op[11:8], 1'b0};
         rvcpu::jal_offset: imm = {{(Width - 21){op[31]}}, op[31], op[19:12], op[20], op[30:21], 1'b0};
+        rvcpu::ui_imm: imm = {op[31:12], 12'b0};
         default: imm = {Width{1'h0}};
     endcase
 end
--- src/rvcpu.sv
+++ src/rvcpu.sv
@@ -1,6 +1,17 @@
 package rvcpu;
 
-typedef enum {auipc_imm, alu_imm, uimm, load_offset, store_offset, jal_offset, jalr_offset, br_offset, none} imm_type_t;
+typedef enum {
+    alu_imm,
+    uimm,
+    load_offset,
+    store_offset,
+    jal_offset,
+    jalr_offset,
+    br_offset,
+    ui_imm,
+    none
+} imm_type_t;
 typedef logic [4:0] reg_t;
+typedef logic [3:0] alu_op_t;
 
 endpackage : rvcpu
